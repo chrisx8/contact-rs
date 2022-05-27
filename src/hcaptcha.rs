2cfb712071e07eaa418dc49323deb6c5ca15eb40
Chris Xiao <30990835+chrisx8@users.noreply.github.com>
2022-05-26 22:45:47 -0400

    hcaptcha secret from env var

--- src/hcaptcha.rs
+++ src/hcaptcha.rs
@@ -1,10 +1,12 @@
 use std::error::Error;
 
 const HCAPTCHA_URL: &str = "https://hcaptcha.com/siteverify";
-const HCAPTCHA_SECRET: &str = "0x0000000000000000000000000000000000000000";
 
 pub async fn validate_hcaptcha(respose: &str) -> Result<(), Box<dyn Error>> {
-    let body = [("secret", HCAPTCHA_SECRET), ("response", respose)];
+    let hcaptcha_secret: &str =
+        option_env!("HCAPTCHA_SECRET").expect("$HCAPTCHA_SECRET is not defined!");
+
+    let body = [("secret", hcaptcha_secret), ("response", respose)];
     let client = reqwest::Client::new();
     let resp = client.post(HCAPTCHA_URL).form(&body).send().await?;
     match resp.error_for_status() {
